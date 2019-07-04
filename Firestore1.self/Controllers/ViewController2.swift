7bd6dac1bbc4ad64a70f119c85bb7a20092d874c
eHime <kinuyamakomaba@gmail.com>
2019-07-04 15:51:16 +0900

    VC2のUIの設定。placefolderやUITextColorなど。Firebaseの直前。次のステップでFirebaseとコネクトする。

--- Firestore1.self/Controllers/ViewController2.swift
+++ Firestore1.self/Controllers/ViewController2.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController, UITextViewDelegate {
     
     
     // UI部品の宣言
@@ -17,20 +17,43 @@ class ViewController2: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var postButton: UIButton!
     
+    // カテゴリー用の変数
+    var category = String()
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        // TextViewとボタンの角を丸くする
+        textView.layer.cornerRadius = 5.0
+        postButton.layer.cornerRadius = 5.0
         
+        // TextViewにplacefolderを追加
+        textView.text = "ここに新規投稿内容を追加"
+        textView.textColor = UIColor.lightGray
+        textView.delegate = self
         
+    }
+    
+    // textViewタップでplacefolderの削除
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+        textView.textColor = UIColor.darkGray
+    }
+    
+    // segmentedControlの値によってcategoryの値を切り替える
+    @IBAction func segmentChanged(_ sender: UISegmentedControl) {
         
     }
     
     
+    // 投稿ボタンを押す＝Firebaseにデータを送信
     @IBAction func postButtonTapped(_ sender: Any) {
+        
+        
+        
+        
     }
     
     
