58ddf9ed04cdc7f1540ca52a34b92ae0a83aa473
eHime <kinuyamakomaba@gmail.com>
2019-07-04 15:33:34 +0900

    VC2のパーツ配置＋ソースコードでUI部品の宣言

---
+++ Firestore1.self/Controllers/ViewController2.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController2.swift
+//  Firestore1.self
+//
+//  Created by 関口大海 on 2019/07/04.
+//  Copyright © 2019 関口大海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    
+    // UI部品の宣言
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var postButton: UIButton!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func postButtonTapped(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+
+}
