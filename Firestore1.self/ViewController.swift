954ef770667b5da3c872752190622ab553dec6e3
eHime <kinuyamakomaba@gmail.com>
2019-07-04 15:11:17 +0900

    Initial Commit

---
+++ Firestore1.self/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firestore1.self
+//
+//  Created by 関口大海 on 2019/07/04.
+//  Copyright © 2019 関口大海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
